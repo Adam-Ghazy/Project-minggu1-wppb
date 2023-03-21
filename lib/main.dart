0cbf966b6871d44668020297b19d63acfe98a30f
Adam <ghozyalfalah02@gmail.com>
2023-03-21 09:15:55 +0700

    Revert "tgl 21"
    This reverts commit f66ca850dc4239dcb4642fb8aa797ec7b0233a2f.

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Text(
-            'HelloNgab',
+            'HelloGuyss',
             style: TextStyle(
               fontSize: 35,
             ),
