6598230e95257b07bd3c23a1b59cab49ef5a11d9
Adam <ghozyalfalah02@gmail.com>
2023-03-21 09:24:22 +0700

    tgl22

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Text(
-            'HelloGuyss',
+            'HelloBang',
             style: TextStyle(
               fontSize: 35,
             ),
